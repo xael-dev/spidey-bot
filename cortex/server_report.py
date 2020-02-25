0b5be07d3a3700481ad3dfeccbcd0b0cfc1f08e7
Alex <alex@itdoctors.com>
2020-02-25 16:25:28 -0500

    Add server_report.py

---
+++ cortex/server_report.py
@@ -0,0 +1,11 @@
+import scrapy
+import logging
+
+logging.getLogger('scrapy').setLevel(logging.WARNING)
+
+class server_tracker(scrapy.Spider):
+    name = 'DownDetector'
+    start_urls = ['https://downdetector.com/status/escape-from-tarkov/']
+
+    def parse (self, response):
+        print (response.css('div.h2.entry-title.color-danger::text').extract())
